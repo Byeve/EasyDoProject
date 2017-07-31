06fc072df9a554061c73cbdfcfe7cf5c95f63d3e
am <anmeng.wei@31huiyi.com>
2017-07-31 10:58:30 +0800

    am

--- EasyDo.RedisCache/RedisDBUtility.cs
+++ EasyDo.RedisCache/RedisDBUtility.cs
@@ -10,7 +10,7 @@ namespace EasyDo.RedisCache
         public RedisDBUtility(EasyDoConfiguration EasyDoConfiguration)
         {
             prcm = CreateManager(EasyDoConfiguration.RedisConnectionStrings(), EasyDoConfiguration.RedisConnectionStrings());
-            ServiceStack.Licensing.RegisterLicense(@"2238-e1JlZjoyMjM4LE5hbWU6MzFIdWlZaSxUeXBlOlJlZGlzQnVzaW5lc3MsSGFzaDp1N01oOGgwdmx0RlJ1UU9LOUZMWEszalNJWVB4UUZtN0tmK3FtdG9sWE9iTGRkdjIrbEtUSTlKYnZkTE90Q2g5QnNDOWFEQTZJTSswdjhucDdjMzVyZUQyVExTYWRFdHNWSzFRV3cxTkhSWjhpWDJFSGhDdit2NHZmM1NYaVIxMDVwTG1iK0ZOZFBKYlpWSzYzK3BOR0tLbldSV3prc2FJd0ZEMmJTbGltdjg9LEV4cGlyeToyMDE2LTAyLTExfQ==");
+           // ServiceStack.Licensing.RegisterLicense(@"2238-e1JlZjoyMjM4LE5hbWU6MzFIdWlZaSxUeXBlOlJlZGlzQnVzaW5lc3MsSGFzaDp1N01oOGgwdmx0RlJ1UU9LOUZMWEszalNJWVB4UUZtN0tmK3FtdG9sWE9iTGRkdjIrbEtUSTlKYnZkTE90Q2g5QnNDOWFEQTZJTSswdjhucDdjMzVyZUQyVExTYWRFdHNWSzFRV3cxTkhSWjhpWDJFSGhDdit2NHZmM1NYaVIxMDVwTG1iK0ZOZFBKYlpWSzYzK3BOR0tLbldSV3prc2FJd0ZEMmJTbGltdjg9LEV4cGlyeToyMDE2LTAyLTExfQ==");
         }
 
         private PooledRedisClientManager CreateManager(string[] readWriteHosts, string[] readOnlyHosts)
